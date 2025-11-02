c12496db8c47ef3888c1ada437b7687b54fab9bc
Kirill Olenev <olenyov.kirill@me.com>
2025-11-02 15:54:44 +0900

    Optimize Reqwest client a bit

--- old-tiles-gen/src/source/reqwest_source.rs
+++ old-tiles-gen/src/source/reqwest_source.rs
@@ -1,16 +1,20 @@
 use error_stack::Report;
 
-pub struct ReqwestSource {}
+pub struct ReqwestSource {
+    client: reqwest::blocking::Client,
+}
 
 impl ReqwestSource {
     pub fn new() -> ReqwestSource {
-        ReqwestSource {}
+        ReqwestSource {
+            client: reqwest::blocking::Client::new(),
+        }
     }
     pub fn get_tile(&self, x: i32, y: i32, z: i32) -> Result<Vec<u8>, Report<reqwest::Error>> {
-        Ok(reqwest::blocking::get(format!(
+        let res = self.client.get(format!(
             "http://ec2-54-252-214-137.ap-southeast-2.compute.amazonaws.com:3000/tile/{x}/{y}/{z}"
-        ))
-        .and_then(|response| response.bytes())?
-        .to_vec())
+        )).send().and_then(|response| response.bytes())?
+        .to_vec();
+        Ok(res)
     }
 }
