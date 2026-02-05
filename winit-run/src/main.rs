97a600296d56e300527545907c9a579ca3031385
Kirill Olenev <olenyov.kirill@me.com>
2026-02-06 08:55:43 +0900

    Continue

--- winit-run/src/main.rs
+++ winit-run/src/main.rs
@@ -1,6 +1,8 @@
-use map::feature_processor::ShashlikFeatureProcessor;
-use map::tiles::shashlik_tiles_provider_v0::ShashlikTilesProviderV0;
+use crate::canvas::SlintWgpuCanvas;
 use map::ShashlikMap;
+use map::feature_processor::ShashlikFeatureProcessor;
+use map::route::RouteCosting;
+use map::tiles::shashlik_tiles_provider_v0::ShashlikTilesProviderV0;
 use osm::source::reqwest_source::ReqwestSource;
 use slint::private_unstable_api::re_exports::PointerEventKind;
 use slint::wgpu_28::{WGPUConfiguration, WGPUSettings};
@@ -8,22 +10,15 @@ use slint::{GraphicsAPI, RenderingState};
 use std::cell::Cell;
 use std::rc::Rc;
 use std::sync::mpsc;
-use wgpu::wgt::TextureViewDescriptor;
-use wgpu::Device;
 use wgpu::Limits;
-use wgpu::Queue;
 use wgpu::SurfaceConfiguration;
-use wgpu::Texture;
 use wgpu::TextureFormat;
 use wgpu::TextureUsages;
-use wgpu::TextureView;
-use map::route::RouteCosting;
-use wgpu_canvas::wgpu_canvas::WgpuCanvas;
 use winit_run::PinchWorkaroundHandler;
 
-slint::include_modules!();
+pub(crate) mod canvas;
 
-struct SlintWgpuCanvas(Queue, Device, SurfaceConfiguration, Texture);
+slint::include_modules!();
 
 enum SlintMapEvent {
     Pan(f32, f32),
@@ -33,42 +28,22 @@ enum SlintMapEvent {
     BtnAction(Action),
 }
 
-impl WgpuCanvas for SlintWgpuCanvas {
-    fn queue(&self) -> &Queue {
-        &self.0
-    }
-
-    fn config(&self) -> &SurfaceConfiguration {
-        &self.2
-    }
-
-    fn device(&self) -> &Device {
-        &self.1
-    }
-
-    fn create_texture_view(&mut self) -> TextureView {
-        self.3.create_view(&TextureViewDescriptor::default())
-    }
-
-    fn present(&mut self) -> Option<Texture> {
-        Some(self.3.clone())
-    }
-
-
-    fn on_resize(&mut self) {}
-}
 fn main() {
+    // TODO Correct UI resize
+    const SCREEN_WIDTH: u32 = 1600;
+    const SCREEN_HEIGHT: u32 = 1200;
+
     env_logger::init();
 
     let (slint_map_event_sender, slint_map_event_receiver) = mpsc::channel();
 
     let pointer_pos = Rc::new(Cell::new((0f32, 0f32)));
-    let pointer_pos2 = Rc::clone(&pointer_pos);
+    let pointer_pos_internal = Rc::clone(&pointer_pos);
     let slint_map_event_sender_internal = slint_map_event_sender.clone();
     let app = PinchWorkaroundHandler::new(move |delta| {
-        let pp = pointer_pos2.get();
+        let (x, y) = pointer_pos_internal.get();
         slint_map_event_sender_internal
-            .send(SlintMapEvent::Pinch(delta * 100.0, pp.0, pp.1))
+            .send(SlintMapEvent::Pinch(delta * 100.0, x, y))
             .unwrap();
     });
 
@@ -81,27 +56,27 @@ fn main() {
         .select()
         .expect("Unable to create Slint backend with WGPU based renderer");
 
-    let ui = AppKiol::new().unwrap();
+    let ui = ShashlikUI::new().unwrap();
     let ui_weak = ui.as_weak();
     let mut shashlik_map = None;
 
     ui.window()
         .set_rendering_notifier(move |state, graphics_api: &GraphicsAPI| {
-            let mut last_pointer_pos = (0f32, 0f32);
             let mut pressed = false;
             match state {
                 RenderingState::RenderingSetup => match graphics_api {
                     GraphicsAPI::WGPU28 { device, queue, .. } => {
-                        let ttt = device.create_texture(&wgpu::TextureDescriptor {
+                        let target_texture = device.create_texture(&wgpu::TextureDescriptor {
                             label: None,
                             size: wgpu::Extent3d {
-                                width: 1600,
-                                height: 1200,
+                                width: SCREEN_WIDTH,
+                                height: SCREEN_HEIGHT,
                                 depth_or_array_layers: 1,
                             },
                             mip_level_count: 1,
                             sample_count: 1,
                             dimension: wgpu::TextureDimension::D2,
+                            // TODO How to use Bgra?
                             format: wgpu::TextureFormat::Rgba8UnormSrgb,
                             usage: wgpu::TextureUsages::RENDER_ATTACHMENT
                                 | wgpu::TextureUsages::TEXTURE_BINDING,
@@ -110,14 +85,15 @@ fn main() {
                         let config = SurfaceConfiguration {
                             usage: TextureUsages::RENDER_ATTACHMENT,
                             format: TextureFormat::Rgba8UnormSrgb,
-                            width: 1600,
-                            height: 1200,
+                            width: SCREEN_WIDTH,
+                            height: SCREEN_HEIGHT,
                             present_mode: Default::default(),
                             desired_maximum_frame_latency: 0,
                             alpha_mode: Default::default(),
                             view_formats: vec![],
                         };
-                        let canvas = SlintWgpuCanvas(queue.clone(), device.clone(), config, ttt);
+                        let canvas =
+                            SlintWgpuCanvas(queue.clone(), device.clone(), config, target_texture);
                         let tiles_provider = ShashlikTilesProviderV0::new(
                             ReqwestSource::new(),
                             ShashlikFeatureProcessor::new(),
@@ -133,17 +109,18 @@ fn main() {
                                 PointerEventKind::Up => pressed = false,
                                 PointerEventKind::Move => {
                                     if pressed {
-                                        let delta_x = -(x - last_pointer_pos.0) / 10.0;
-                                        let delta_y = -(y - last_pointer_pos.1) / 10.0;
+                                        let delta_x = -(x - pointer_pos.get().0) / 10.0;
+                                        let delta_y = -(y - pointer_pos.get().1) / 10.0;
                                         slint_map_event_sender_internal
                                             .send(SlintMapEvent::Pan(delta_x, delta_y))
                                             .unwrap();
                                     }
                                     pointer_pos.set((x, y));
-                                    last_pointer_pos = (x, y);
                                 }
                                 _ => {}
                             });
+
+                            // TODO How to get rid of all this clones?
                             let slint_map_event_sender_internal = slint_map_event_sender.clone();
                             ui_weak.on_vert_scroll(move |delta_y| {
                                 slint_map_event_sender_internal
@@ -160,15 +137,17 @@ fn main() {
 
                             let slint_map_event_sender_internal = slint_map_event_sender.clone();
                             ui_weak.on_btn_click(move |action| {
-                                println!("click {:?}",action);
-                                slint_map_event_sender_internal.send(SlintMapEvent::BtnAction(action)).unwrap()
+                                println!("click {:?}", action);
+                                slint_map_event_sender_internal
+                                    .send(SlintMapEvent::BtnAction(action))
+                                    .unwrap()
                             });
                         }
 
                         let mut map =
                             pollster::block_on(ShashlikMap::new(Box::new(canvas), tiles_provider))
                                 .unwrap();
-                        map.resize(1600, 1200);
+                        map.resize(SCREEN_WIDTH, SCREEN_HEIGHT);
                         shashlik_map = Some(map);
                     }
                     _ => {}
@@ -190,19 +169,24 @@ fn main() {
                                 }
                                 SlintMapEvent::FollowMode(enabled) => {
                                     shashlik_map.set_camera_follow_mode(enabled);
-                                },
-                                SlintMapEvent::BtnAction(action) => {
-                                    println!("clicked {:?}",action);
-                                    match action {
-                                        Action::DmOffice => {
-                                            shashlik_map.set_lon_lat_bearing( 139.74777078320227, 35.62298925839326, Some(0f32));
-                                        },
-                                        Action::Route => {
-                                            shashlik_map.create_route_to_from_screen_center(RouteCosting::Motorbike);
-                                        },
-                                        Action::KML => {},
-                                    }
                                 }
+                                SlintMapEvent::BtnAction(action) => match action {
+                                    Action::DmOffice => {
+                                        shashlik_map.set_lon_lat_bearing(
+                                            139.74777078320227,
+                                            35.62298925839326,
+                                            Some(0f32),
+                                        );
+                                    }
+                                    Action::Route => {
+                                        shashlik_map.create_route_to_from_screen_center(
+                                            RouteCosting::Motorbike,
+                                        );
+                                    }
+                                    Action::KML => {
+                                        // TODO Fix KML loading after fixing georust KML
+                                    }
+                                },
                             };
                         }
                         let target_texture = shashlik_map.update_and_render();
