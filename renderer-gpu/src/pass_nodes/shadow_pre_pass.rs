aa88ecd0de2d3326dc9d82b9edc200328ed3b3da
Kirill Olenev <olenyov.kirill@me.com>
2026-08-15 22:19:05 +0900

    Minor shadow map fixes

--- renderer-gpu/src/pass_nodes/shadow_pre_pass.rs
+++ renderer-gpu/src/pass_nodes/shadow_pre_pass.rs
@@ -26,25 +26,27 @@ impl PassNode for ShadowPrepass {
         layers: &mut Layers,
         global_context: &mut GlobalContext,
     ) {
-        let depth_attachment = wgpu::RenderPassDepthStencilAttachment {
-            view: global_context.texture_view_resources.get_or_unwrap(TextureViewKind::ShadowMapDepth),
-            depth_ops: Some(wgpu::Operations {
-                load: wgpu::LoadOp::Clear(1.0),
-                store: wgpu::StoreOp::Store,
-            }),
-            stencil_ops: None,
-        };
+        if global_context.is_shadow_mapping_enabled() {
+            let depth_attachment = wgpu::RenderPassDepthStencilAttachment {
+                view: global_context.texture_view_resources.get_or_unwrap(TextureViewKind::ShadowMapDepth),
+                depth_ops: Some(wgpu::Operations {
+                    load: wgpu::LoadOp::Clear(1.0),
+                    store: wgpu::StoreOp::Store,
+                }),
+                stencil_ops: None,
+            };
 
-        let descriptor = wgpu::RenderPassDescriptor {
-            label: Some("Shadow Render Pass"),
-            color_attachments: &[],
-            depth_stencil_attachment: Some(depth_attachment),
-            occlusion_query_set: None,
-            timestamp_writes: None,
-            multiview_mask: None,
-        };
+            let descriptor = wgpu::RenderPassDescriptor {
+                label: Some("Shadow Render Pass"),
+                color_attachments: &[],
+                depth_stencil_attachment: Some(depth_attachment),
+                occlusion_query_set: None,
+                timestamp_writes: None,
+                multiview_mask: None,
+            };
 
-        let mut render_pass = encoder.begin_render_pass(&descriptor);
-        layers.mesh_layer.render_new(&mut render_pass, &mut self.fill_shadow_map_pipeline, global_context)
+            let mut render_pass = encoder.begin_render_pass(&descriptor);
+            layers.mesh_layer.render_new(&mut render_pass, &mut self.fill_shadow_map_pipeline, global_context)
+        }
     }
 }
--- renderer-gpu/src/view_projection.rs
+++ renderer-gpu/src/view_projection.rs
@@ -1,10 +1,10 @@
-use crate::{GpuRenderer, RendererUpdateData};
 use crate::Renderer;
-use geo_types::{coord, Coord};
-use glam::{DMat4, DVec2, DVec3, DVec4, Mat4, Vec2, Vec4Swizzles};
-use renderer_common::{min_f64, LIGHT_POS, max_f64};
-use wgpu::{Buffer, Device, Queue, SurfaceConfiguration};
 use crate::render_config::RenderConfig;
+use crate::{GpuRenderer, RendererUpdateData};
+use geo_types::{Coord, coord};
+use glam::{DMat4, DVec2, DVec3, DVec4, Mat4, Vec2, Vec4Swizzles};
+use renderer_common::{LIGHT_POS, max_f64, min_f64};
+use wgpu::{Buffer, Device, Queue, SurfaceConfiguration};
 
 #[rustfmt::skip]
 const FLIP_Y: DMat4 = DMat4::from_cols_array(
@@ -100,7 +100,8 @@ impl ViewProjection {
             .to_cols_array_2d();
         let view_proj = FLIP_Y * data.view_proj_matrix;
 
-        self.ortho_for_shadow_map(&mut data.view_light_matrix);
+
+        self.ortho_for_shadow_map(&mut data.view_light_matrix, data.scale);
 
         self.uniform.light_view_proj = (self.ortho * data.view_light_matrix)
             .as_mat4()
@@ -137,7 +138,7 @@ impl ViewProjection {
     }
 
     /// calculate ortho matrix for shadow mapping
-    fn ortho_for_shadow_map(&mut self, view_light_matrix: &mut DMat4) {
+    fn ortho_for_shadow_map(&mut self, view_light_matrix: &mut DMat4, scale: f32) {
         if !self.is_shadow_mapping_enabled() {
             return;
         }
@@ -147,17 +148,19 @@ impl ViewProjection {
         let c4 = self.clip_to_world(&coord! {x: 1.0, y: 1.0});
         if let (Some(c1), Some(c2), Some(c3), Some(c4)) = (c1, c2, c3, c4) {
             let center = ((c1 + c2 + c3 + c4) / 4.0).extend(0.0);
-            let light_view = DMat4::look_at_rh(center + LIGHT_POS, center, DVec3::Z);
+            let light_view = DMat4::look_at_rh(center + 1.0 * LIGHT_POS, center, DVec3::Z);
 
             let p1 = light_view.transform_point3(c1.extend(0.0));
             let p2 = light_view.transform_point3(c2.extend(0.0));
             let p3 = light_view.transform_point3(c3.extend(0.0));
             let p4 = light_view.transform_point3(c4.extend(0.0));
 
-            let min_x = min_f64!(p1.x, p2.x, p3.x, p4.x) - Self::MAX_MESH_HEIGHT;
-            let min_y = min_f64!(p1.y, p2.y, p3.y, p4.y) - Self::MAX_MESH_HEIGHT;
-            let max_x = max_f64!(p1.x, p2.x, p3.x, p4.x) + Self::MAX_MESH_HEIGHT;
-            let max_y = max_f64!(p1.y, p2.y, p3.y, p4.y) + Self::MAX_MESH_HEIGHT;
+            // dynamically change bounds, so MAX_MESH_HEIGHT won't affect much the matrix size when close to ground
+            let max_mesh_height = (Self::MAX_MESH_HEIGHT * (1.5 * scale as f64)).min(Self::MAX_MESH_HEIGHT);
+            let min_x = min_f64!(p1.x, p2.x, p3.x, p4.x) - max_mesh_height;
+            let min_y = min_f64!(p1.y, p2.y, p3.y, p4.y) - max_mesh_height;
+            let max_x = max_f64!(p1.x, p2.x, p3.x, p4.x) + max_mesh_height;
+            let max_y = max_f64!(p1.y, p2.y, p3.y, p4.y) + max_mesh_height;
 
             let depth_texture_size = self.shadow_texture_size.0 as f64;
             let ortho_width = ((max_x - min_x) / Self::ORTHO_STEP).round() * Self::ORTHO_STEP;
