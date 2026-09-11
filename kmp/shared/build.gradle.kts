6da974199959c886b0047c28c10d51bae3f0f648
Kirill Olenev <Olenyov.Kirill@me.com>
2026-09-12 08:37:17 +0900

    Temporary disable iOS targets (#227)

--- kmp/shared/build.gradle.kts
+++ kmp/shared/build.gradle.kts
@@ -76,15 +76,18 @@ kotlin {
         }
     }
 
-    listOf(
-        iosArm64(),
-        iosSimulatorArm64()
-    ).forEach { iosTarget ->
-        iosTarget.binaries.framework {
-            baseName = "Shared"
-            isStatic = true
-        }
-    }
+    // TODO iOS targe temporary disabled.
+    //  It takes a lot of time to build locally and on CI and produces huge binaries(MVN complains)
+    //  Need to figure out the reason later.
+//    listOf(
+//        iosArm64(),
+//        iosSimulatorArm64()
+//    ).forEach { iosTarget ->
+//        iosTarget.binaries.framework {
+//            baseName = "Shared"
+//            isStatic = true
+//        }
+//    }
 
     sourceSets {
         androidMain.dependencies {
