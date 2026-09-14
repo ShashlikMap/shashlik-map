bbf0e9db0cfd975093568a43ca3d24fdbda2c7a6
Kirill Olenev <olenyov.kirill@me.com>
2026-09-15 08:37:49 +0900

    Update default tile store for demo app and headless mode

--- kmp/demo/composeApp/src/commonMain/kotlin/com/shashlik/demo/App.kt
+++ kmp/demo/composeApp/src/commonMain/kotlin/com/shashlik/demo/App.kt
@@ -149,7 +149,7 @@ fun App() {
                 }
             }
         ) {
-            var mvtCheckedState by remember { mutableStateOf(false) }
+            var mvtCheckedState by remember { mutableStateOf(true) }
             val shapes = remember { mutableStateListOf<Triple<List<Point>, ShapeType, Color>>() }
             ShashlikMap(withAutoLocationEvent = true, mvtTiles = mvtCheckedState) {
                 shapes.forEach { shape ->
