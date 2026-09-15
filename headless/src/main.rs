028edcf7f4df256b8deed98e600bfc622195e494
Kirill Olenev <olenyov.kirill@me.com>
2026-09-16 07:58:30 +0900

    Extend headless mode and screenshot testing

--- headless/src/main.rs
+++ headless/src/main.rs
@@ -1,3 +1,4 @@
+use std::env;
 use map::feature_processor::ShashlikFeatureProcessor;
 use map::tiles::default_tiles_provider::DefaultTilesProvider;
 use map::{DEFAULT_FONT_DATA, ShashlikMap};
@@ -9,19 +10,32 @@ use renderer_gpu::render_config::RenderConfig;
 use renderer_gpu::wgpu_canvas::DefaultWgpuCanvas;
 use std::thread::sleep;
 use std::time::Duration;
+use geo_types::Point;
 use wgpu::{
     Device, DeviceDescriptor, Features, Label, Limits, MemoryHints, PowerPreference, Queue,
 };
+use map::overlay::ShapeType;
 
 fn main() {
-    println!("Headless mode started.....");
+    let args: Vec<String> = env::args().collect();
+    let is_globe = args.len() > 1 && args.contains(&"-globe".to_string());
+    let with_overlay = args.len() > 1 && args.contains(&"-overlay".to_string());
+    println!("Headless mode started, is_globe: {}, with_overlay: {}", is_globe, with_overlay);
     let (device, queue) = pollster::block_on(async { create_wgpu().await });
 
     // the size is what used for macOS is this time
-    let size = wgpu::Extent3d {
-        width: 1561u32,
-        height: 1168u32,
-        depth_or_array_layers: 1,
+    let size = if !is_globe {
+        wgpu::Extent3d {
+            width: 1561u32,
+            height: 1168u32,
+            depth_or_array_layers: 1,
+        }
+    } else {
+        wgpu::Extent3d {
+            width: 1080u32,
+            height: 1920u32,
+            depth_or_array_layers: 1,
+        }
     };
     let target_texture = device.create_texture(&wgpu::TextureDescriptor {
         label: None,
@@ -60,13 +74,30 @@ fn main() {
     map.resize(size.width, size.height);
     map.set_camera_follow_mode(true);
     map.set_cam_follow_zoom_lock(None);
-    map.zoom_delta(1.15, (0.0, 0.0));
-    map.set_lon_lat_bearing(139.757080078125, 35.69100828125, None);
+    if is_globe {
+        map.zoom_delta(0.000035, (0.0, 0.0));
+        map.set_lon_lat_bearing(105.757080078125, 25.69100828125, None);
+    } else {
+        map.zoom_delta(1.15, (0.0, 0.0));
+        map.set_lon_lat_bearing(139.757080078125, 35.69100828125, None);
+        if with_overlay {
+            let converter = map.create_location_coord_converter();
+            map.overlay().add_overlay_shape(converter, vec![Point::new(139.757080078125, 35.69100828125),
+                                                            Point::new(139.757780078125, 35.69190828125),
+                                                            Point::new(139.757680078125, 35.69050828125)], ShapeType::Polygon,
+                                            [0.0, 0.0, 1.0]);
+            let converter = map.create_location_coord_converter();
+            map.overlay().add_overlay_shape(converter, vec![Point::new(139.757080078125, 35.69100828125),
+                                                            Point::new(139.757780078125, 35.69190828125)], ShapeType::Line,
+                                            [1.0, 0.0, 0.0]);
+        }
+    }
 
     println!("Headless mode. Run frames");
+    let frames_to_run = if is_globe { 300 } else { 120 };
     sleep(Duration::from_secs(1));
     map.update_and_render(());
-    for _ in 0..120 {
+    for _ in 0..frames_to_run {
         sleep(Duration::from_millis(16));
         map.update_and_render(());
     }
