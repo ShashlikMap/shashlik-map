f2c83adf6007b0bf2f52c82fd181a567987a572a
Kirill Olenev <olenyov.kirill@me.com>
2026-09-16 10:59:09 +0900

    Fix compilation

--- map/src/route/route_controller.rs
+++ map/src/route/route_controller.rs
@@ -49,7 +49,7 @@ impl<RAPI: RendererApi + 'static> RouteController<RAPI> {
         #[cfg(target_os = "linux")]
         {
             let route: Vec<Point> = vec![point!(x:0.0, y:0.0), point!(x: 1.0, y:0.0)];
-            let route = Box::new(OverlayShapeGroup::new(route, "route_layer".to_string(), StyleId::new("route"), ShapeType::Line));
+            let route = Box::new(OverlayShapeGroup::new(route, "route_layer".to_string(), StyleId::new("route"), ShapeType::Line(None)));
             self.api
                 .add_render_group("route".to_string(), route.spatial_data(), route);
         }
