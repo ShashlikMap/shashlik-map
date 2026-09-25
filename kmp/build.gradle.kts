33919d3e55e8a903f9c99687ea0ca85a8eaa4d2e
Kirill Olenev <olenyov.kirill@me.com>
2026-09-25 09:24:18 +0900

    Update Rustls

--- kmp/build.gradle.kts
+++ kmp/build.gradle.kts
@@ -9,3 +9,44 @@ plugins {
     alias(libs.plugins.kotlinAndroid) apply false
     alias(libs.plugins.vanniktech.mavenPublish) apply false
 }
+
+abstract class RustlsVersion : ValueSource<String, RustlsVersion.Params> {
+    interface Params : ValueSourceParameters {
+        val lockFile: RegularFileProperty
+    }
+
+    companion object {
+        const val CRATE_NAME = "rustls-platform-verifier-android"
+    }
+
+    override fun obtain(): String {
+        val version = parameters.lockFile.get().asFile.readLines().let { lines ->
+            val nameIdx = lines.indexOfFirst { it.trim() == "name = \"$CRATE_NAME\"" }
+            if (nameIdx < 0) {
+                null
+            } else {
+                lines.drop(nameIdx + 1)
+                    .firstOrNull { it.trimStart().startsWith("version = ") }
+                    ?.substringAfter('"', "")
+                    ?.substringBefore('"', "")
+                    ?.takeIf { it.isNotEmpty() }
+            }
+        }
+        return version ?: error("$CRATE_NAME not found in Cargo.lock")
+    }
+}
+
+val rustlsPlatformVerifierVersion = providers.of(RustlsVersion::class.java) {
+    parameters.lockFile.set(layout.projectDirectory.file("../Cargo.lock"))
+}
+
+allprojects {
+    configurations.configureEach {
+        resolutionStrategy.eachDependency {
+            if (requested.group == "org.rustls" && requested.name == "rustls-platform-verifier") {
+                useVersion(rustlsPlatformVerifierVersion.get())
+                because("native component version must be identical to version of ${RustlsVersion.CRATE_NAME}")
+            }
+        }
+    }
+}
